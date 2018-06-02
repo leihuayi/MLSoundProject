6b61b50195e12c8eaeb940dbc92b32946d510c83
Usama Zafar <uzafar92@live.com>
2018-06-02 14:52:18 +0800

    convo + cleaned code

--- code/features.py
+++ code/features.py
@@ -16,12 +16,11 @@ import pandas as pd
 #   Define global parameters to be used through out the program                                 #
 #                                                                                               #
 #-----------------------------------------------------------------------------------------------#
-TR_FEATURE_NPY = os.path.join(os.path.dirname(__file__),"../data/f1.npy")
-TR_LABEL_NPY = os.path.join(os.path.dirname(__file__),"../data/f2.npy")
-TR_VERIFY_NPY = os.path.join(os.path.dirname(__file__),"../data/f3.npy")
-TS_FEATURE_NPY = os.path.join(os.path.dirname(__file__),"../data/f4.npy")
-TS_F_NAME_NPY = os.path.join(os.path.dirname(__file__),"../data/f5.npy")
-LABEL_DICT_NPY = os.path.join(os.path.dirname(__file__),"../data/f6.txt")
+TR_FEATURE_NPY = os.path.join(os.path.dirname(__file__),"../data/f1_{0}.npy")
+TR_LABEL_NPY = os.path.join(os.path.dirname(__file__),"../data/f2_{0}.npy")
+TS_FEATURE_NPY = os.path.join(os.path.dirname(__file__),"../data/f3_{0}.npy")
+TS_F_NAME_NPY = os.path.join(os.path.dirname(__file__),"../data/f4_{0}.npy")
+LABEL_DICT_NPY = os.path.join(os.path.dirname(__file__),"../data/f5.txt")
 
 CHUNK_SIZE = 500
 FEATURE_SIZE = 193
@@ -79,10 +78,10 @@ def p_train_thread(audio_path, label_dictionary, data):
 #***********************************************************************************************#
 #                                                                                               #
 #   Module:                                                                                     #
-#   p_train_thread()                                                                            #
+#   p_predict_thread()                                                                          #
 #                                                                                               #
 #   Description:                                                                                #
-#   Internal function to parse training audio files in multi-threaded environment.              #
+#   Internal function to parse prediction audio files in multi-threaded environment.            #
 #                                                                                               #
 #***********************************************************************************************#
 def p_predict_thread(audio_path, name_list):
@@ -108,43 +107,32 @@ def p_predict_thread(audio_path, name_list):
 #   Parses audio data that needs to be predicted upon.                                          #
 #                                                                                               #
 #***********************************************************************************************#
-def parse_audio_files_predict(audio_path, file_ext="*.wav", nn_type):
-    
+def parse_audio_files_predict(audio_path, file_ext="*.wav", nn_type): 
     # initialize variables
-    features = np.empty((0,FEATURE_SIZE))
-    
+    features = np.empty((0,FEATURE_SIZE)) 
     # get the list of files in the audio folder
-    name_list = os.listdir(audio_path)
-    
+    name_list = os.listdir(audio_path) 
     # create a thread pool to process the workload
-    thread_pool = []
-    
+    thread_pool = [] 
     # split the filename list into chunks of 'CHUNK_SIZE' files each
-    data = utils.generate_chunks(name_list, CHUNK_SIZE)
-    
+    data = utils.generate_chunks(name_list, CHUNK_SIZE) 
     # each chunk is the amount of data that will be processed by a single thread
     for chunk in data:
         if nn_type == 0:
             thread_pool.append(utils.ThreadWithReturnValue(target=p_predict_thread, args=(audio_path, chunk)))
         else:
             thread_pool.append(utils.ThreadWithReturnValue(target=p_predict_cnn_thread, args=(audio_path, chunk)))
-        
-    
     # print a log message for status update
     utils.write_log_msg("PREDICT: creating a total of {0} threads...".format(len(thread_pool)))  
-    
     # start the entire thread pool
     for single_thread in thread_pool:
         single_thread.start()
-    
     # wait for thread pool to return their results of processing
     for single_thread in thread_pool:
         ft = single_thread.join()
         features = np.vstack([features,ft])
-    
     # perform final touches to extracted arrays
     features = np.array(features)
-
     # normalize data
     #mean = np.mean(features, axis=0)
     #std = np.std(features, axis=0)
@@ -164,41 +152,31 @@ def parse_audio_files_predict(audio_path, file_ext="*.wav", nn_type):
 #***********************************************************************************************#
 def parse_audio_files_train(audio_path, train_csv_path, label_dictionary, file_ext="*.wav", nn_type):
     # initialize variables
-    features, labels, verified = np.empty((0,FEATURE_SIZE)), np.empty(0), np.empty(0)    
-    
+    features, labels = np.empty((0,FEATURE_SIZE)), np.empty(0)  
     # read audio files using pandas and split it into chunks of 'CHUNK_SIZE' files each
     data = pd.read_csv(train_csv_path, chunksize=CHUNK_SIZE)
-    
     # create a thread pool to process the workload
     thread_pool = []
-        
     # each chunk is the amount of data that will be processed by a single thread
     for chunk in data:
         if(nn_type == 0):
             thread_pool.append(utils.ThreadWithReturnValue(target=p_train_thread, args=(audio_path, label_dictionary, chunk)))
         else:
             thread_pool.append(utils.ThreadWithReturnValue(target=p_train_cnn_thread, args=(audio_path, label_dictionary, chunk)))
-        
-    
     # print a log message for status update
     utils.write_log_msg("TRAIN: creating a total of {0} threads...".format(len(thread_pool)))  
-    
     # start the entire thread pool
     for single_thread in thread_pool:
         single_thread.start()
-    
     # wait for thread pool to return their results of processing
     for single_thread in thread_pool:
-        ft, lbl, stat = single_thread.join()
+        ft, lbl = single_thread.join()
         features = np.vstack([features,ft])
         labels = np.append(labels, lbl)
-        verified = np.append(verified, stat)
-    
     # perform final touches to extracted arrays
     features = np.array(features)
     labels = one_hot_encode(np.array(labels, dtype = np.int))
     verified = np.array(verified, dtype=np.bool)
-
     # normalize data
     #mean = np.mean(features, axis=0)
     #std = np.std(features, axis=0)
@@ -226,47 +204,10 @@ def one_hot_encode(labels):
 #***********************************************************************************************#
 #                                                                                               #
 #   Module:                                                                                     #
-#   read_features()                                                                             #
+#   windows()                                                                                   #
 #                                                                                               #
 #   Description:                                                                                #
-#   Program responsible for reading pre-made features files without any extraction.             #
-#                                                                                               #
-#***********************************************************************************************#
-def read_features():
-    tr_features = np.load(TR_FEATURE_NPY)
-    tr_labels = np.load(TR_LABEL_NPY)
-    ts_features = np.load(TS_FEATURE_NPY)
-    tr_verified = np.load(TR_VERIFY_NPY)
-    dictionary = json.load(open(LABEL_DICT_NPY))
-    ts_name_list = pickle.load(open(TS_F_NAME_NPY, "rb"))
-    return dictionary, tr_features, tr_labels, tr_verified, ts_features, ts_name_list
-
-#***********************************************************************************************#
-#                                                                                               #
-#   Module:                                                                                     #
-#   store_features()                                                                            #
-#                                                                                               #
-#   Description:                                                                                #
-#   Program responsible for storing pre-made features to files.                                 #
-#                                                                                               #
-#***********************************************************************************************#
-def store_features(dictionary, tr_features, tr_labels, tr_verified, ts_features, ts_name_list):
-    np.save(TR_FEATURE_NPY, tr_features)
-    np.save(TR_LABEL_NPY, tr_labels)
-    np.save(TS_FEATURE_NPY, ts_features)
-    np.save(TR_VERIFY_NPY, tr_verified)
-    json.dump(dictionary, open(LABEL_DICT_NPY,'w'))
-    pickle.dump(ts_name_list, open(TS_F_NAME_NPY, "wb"))
-
-
-
-#***********************************************************************************************#
-#                                                                                               #
-#   Module:                                                                                     #
-#   p_train_cnn_thread()                                                                        #
-#                                                                                               #
-#   Description:                                                                                #
-#   Internal function to parse training audio files in multi-threaded environment for CNN       #
+#   Function to create a window on audio data.                                                  #
 #                                                                                               #
 #***********************************************************************************************#
 def windows(data, window_size):
@@ -275,6 +216,15 @@ def windows(data, window_size):
         yield start, start + window_size
         start += (window_size / 2)
 
+#***********************************************************************************************#
+#                                                                                               #
+#   Module:                                                                                     #
+#   p_train_cnn_thread()                                                                        #
+#                                                                                               #
+#   Description:                                                                                #
+#   Internal function to parse training audio files in multi-threaded environment for CNN.      #
+#                                                                                               #
+#***********************************************************************************************#
 def p_train_cnn_thread(audio_path, label_dictionary, data, bands = 60, frames = 41):
     # initialize variables
     features, labels = np.empty((0,FEATURE_SIZE)), np.empty(0)   
@@ -303,6 +253,15 @@ def p_train_cnn_thread(audio_path, label_dictionary, data, bands = 60, frames = 
     # return the extracted features to the calling program
     return np.array(features), labels
 
+#***********************************************************************************************#
+#                                                                                               #
+#   Module:                                                                                     #
+#   p_predict_cnn_thread()                                                                      #
+#                                                                                               #
+#   Description:                                                                                #
+#   Internal function to parse prediction audio files in multi-threaded environment for CNN.    #
+#                                                                                               #
+#***********************************************************************************************#
 def p_predict_cnn_thread(audio_path, name_list, bands = 60, frames = 41):
     # initialize variables
     features = np.empty((0,FEATURE_SIZE))
@@ -322,4 +281,54 @@ def p_predict_cnn_thread(audio_path, name_list, bands = 60, frames = 41):
     for i in range(len(features)):
         features[i, :, :, 1] = librosa.feature.delta(features[i, :, :, 0])
             
+    # return the extracted features to the calling program
     return np.array(features)
+
+#***********************************************************************************************#
+#                                                                                               #
+#   Module:                                                                                     #
+#   read_features()                                                                             #
+#                                                                                               #
+#   Description:                                                                                #
+#   Program responsible for reading pre-made features files without any extraction.             #
+#                                                                                               #
+#***********************************************************************************************#
+def read_features():
+    # read mnn features
+    tr_mnn_features = np.load(TR_FEATURE_NPY.format("mnn"))
+    tr_mnn_labels = np.load(TR_LABEL_NPY.format("mnn"))
+    ts_mnn_features = np.load(TS_FEATURE_NPY.format("mnn"))
+    ts_mnn_name_list = pickle.load(open(TS_F_NAME_NPY.format("mnn"), "rb"))
+    # read cnn features
+    tr_cnn_features = np.load(TR_FEATURE_NPY.format("cnn"))
+    tr_cnn_labels = np.load(TR_LABEL_NPY.format("cnn"))
+    ts_cnn_features = np.load(TS_FEATURE_NPY.format("cnn"))
+    ts_cnn_name_list = pickle.load(open(TS_F_NAME_NPY.format("cnn"), "rb"))
+    # read the dictionary which is same in both cases
+    dictionary = json.load(open(LABEL_DICT_NPY))
+    # return the read values
+    return dictionary, tr_mnn_features, tr_mnn_labels, ts_mnn_features, ts_mnn_name_list, tr_cnn_features, tr_cnn_labels, ts_cnn_features, ts_cnn_name_list
+
+#***********************************************************************************************#
+#                                                                                               #
+#   Module:                                                                                     #
+#   store_features()                                                                            #
+#                                                                                               #
+#   Description:                                                                                #
+#   Program responsible for storing pre-made features to files.                                 #
+#                                                                                               #
+#***********************************************************************************************#
+def store_features(dictionary, tr_mnn_features, tr_mnn_labels, ts_mnn_features, ts_mnn_name_list, tr_cnn_features, tr_cnn_labels, ts_cnn_features, ts_cnn_name_list):
+    # save mnn features
+    np.save(TR_FEATURE_NPY.format("mnn"), tr_mnn_features)
+    np.save(TR_LABEL_NPY.format("mnn"), tr_mnn_labels)
+    np.save(TS_FEATURE_NPY.format("mnn"), ts_mnn_features)
+    pickle.dump(ts_mnn_name_list, open(TS_F_NAME_NPY.format("mnn"), "wb"))
+    # save cnn features
+    np.save(TR_FEATURE_NPY.format("cnn"), tr_cnn_features)
+    np.save(TR_LABEL_NPY.format("cnn"), tr_cnn_labels)
+    np.save(TS_FEATURE_NPY.format("cnn"), ts_cnn_features)
+    pickle.dump(ts_mnn_name_list, open(TS_F_NAME_NPY.format("cnn"), "wb"))
+    # save the dictionary which is same in both cases
+    json.dump(dictionary, open(LABEL_DICT_NPY,'w'))
+    
--- code/mlsound.py
+++ code/mlsound.py
@@ -29,28 +29,45 @@ OUTPUT_CSV = os.path.join(os.path.dirname(__file__),"../data/submission.csv")
 #                                                                                               #
 #***********************************************************************************************#
 def read_audio_files():
+    
     # print a log message for status update
     utils.write_log_msg("creating data dictionary...")  
+    
     # create a dictionary from the provided train.csv file
     dictionary = utils.create_dictionary(TRAIN_CSV)  
+    
     # print a log message for status update
     utils.write_log_msg("extracting features of training data...")  
     # call the feature extraction module to get audio features
-    tr_features, tr_labels, tr_verified =  features.parse_audio_files_train(TRAIN_AUDIO_PATH,TRAIN_CSV,dictionary,1)  
+    tr_mnn_features, tr_mnn_labels =  features.parse_audio_files_train(TRAIN_AUDIO_PATH,TRAIN_CSV,dictionary, 0)  
+    # call the feature extraction module to get audio features
+    tr_cnn_features, tr_cnn_labels =  features.parse_audio_files_train(TRAIN_AUDIO_PATH,TRAIN_CSV,dictionary, 1)  
+    
     # print a log message for status update
-    utils.write_log_msg("processed {0} files of training data...".format(len(tr_features)))  
+    utils.write_log_msg("processed {0} files of training data for mnn...".format(len(tr_mnn_features)))  
+    # print a log message for status update
+    utils.write_log_msg("processed {0} files of training data for cnn...".format(len(tr_cnn_features)))  
+    
     # print a log message for status update
     utils.write_log_msg("extracting features of prediction data...")  
     # call the feature extraction module to get audio features
-    ts_features, ts_name_list = features.parse_audio_files_predict(TEST_AUDIO_PATH,1)  
+    ts_mnn_features, ts_mnn_name_list = features.parse_audio_files_predict(TEST_AUDIO_PATH, 0)  
+    # call the feature extraction module to get audio features
+    ts_cnn_features, ts_cnn_name_list = features.parse_audio_files_predict(TEST_AUDIO_PATH, 1)  
+    
     # print a log message for status update
-    utils.write_log_msg("processed {0} files of prediction data...".format(len(ts_features)))
+    utils.write_log_msg("processed {0} files of prediction data for mnn...".format(len(ts_mnn_features)))
+    # print a log message for status update
+    utils.write_log_msg("processed {0} files of prediction data for cnn...".format(len(ts_cnn_features)))
+    
     # print a log message for status update
     utils.write_log_msg("storing features for future use...")
     # store features so that they can be used in future
-    features.store_features(dictionary, tr_features, tr_labels, tr_verified, ts_features, ts_name_list)
+    features.store_features(dictionary, tr_mnn_features, tr_mnn_labels, ts_mnn_features, ts_mnn_name_list,
+                                        tr_cnn_features, tr_cnn_labels, ts_cnn_features, ts_cnn_name_list)
+    
     # return the results to calling program
-    return dictionary, tr_features, tr_labels, tr_verified, ts_features, ts_name_list
+    return dictionary, tr_mnn_features, tr_mnn_labels, ts_mnn_features, ts_mnn_name_list, tr_cnn_features, tr_cnn_labels, ts_cnn_features, ts_cnn_name_list
 
 #***********************************************************************************************#
 #                                                                                               #
@@ -66,17 +83,17 @@ def main(_load = True):
     utils.initialize_log()  
     # read audio files and parse them or simply load from pre-extracted feature files
     if _load:
-        dictionary, tr_features, tr_labels, tr_verified, ts_features, ts_name_list = read_audio_files()  
+        dictionary, tr_mnn_features, tr_mnn_labels, ts_mnn_features, ts_mnn_name_list, tr_cnn_features, tr_cnn_labels, ts_cnn_features, ts_cnn_name_list  = read_audio_files()  
     else:
-        dictionary, tr_features, tr_labels, tr_verified, ts_features, ts_name_list = features.read_features()
+        dictionary, tr_mnn_features, tr_mnn_labels, ts_mnn_features, ts_mnn_name_list, tr_cnn_features, tr_cnn_labels, ts_cnn_features, ts_cnn_name_list  = features.read_features()
     # print a log message for status update
     utils.write_log_msg("starting multi-layer neural network training...")
     # use the above extracted features for the training of the model
-    predictions_top3 = train.train(tr_features, tr_labels, ts_features, n_classes=len(dictionary))
+    predictions_top3 = train.train(tr_mnn_features, tr_mnn_labels, ts_mnn_features, tr_cnn_features, tr_cnn_labels, ts_cnn_features, n_classes=len(dictionary))
     # print a log message for status update
     utils.write_log_msg("outputing prediction results to a csv file...")
     # print the predicted results to a csv file.
-    utils.print_csv_file(predictions_top3, ts_name_list, dictionary, OUTPUT_CSV)
+    utils.print_csv_file(predictions_top3, ts_mnn_name_list, ts_cnn_name_list, dictionary, OUTPUT_CSV)
     # print a log message for status update
     utils.write_log_msg("done...")
 
